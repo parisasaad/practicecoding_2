afe1569c9a031b2e92434bbd2202395d8e3551c1
parisasaad <58298188+parisasaad@users.noreply.github.com>
2022-05-03 19:58:14 -0400

    Update Exercises_2.py

--- Exercises_2.py
+++ Exercises_2.py
@@ -1,34 +1,15 @@
-##########Kamyar##########
-val1 = float(input("Enter first variable's value: "))
-val2 = float(input("Enter second variable's value: "))
-minus=val1-val2
-sum=val1+val2
-multiplication=val1*val2
-power=val1**val2
-
-print("minus=",minus)
-print("sum=",sum)
-print("multiplication=",multiplication)
-print("power=",power)
-
-########Parisa########
-val3 = float(input("Enter third variable's value: "))
-minus=minus-val3
-sum=sum+val3
-multiplication=multiplication*val3
-power=power**val3
-
-print(" peer minus=",minus)
-print("peer sum=",sum)
-print("peer multiplication=",multiplication)
-print("peer power=",power)
-
-
 #Ask a user for two inputs
 #Implement  (- , + , *, **, / , operations on them) and print the results.
 #ubmit your code to GitHub Provide access to your peer to modify your code
 #Ask your peer add the third input and apply its value to the output of previous operations, and print it. E.g., (2+2=4  new input Is 2  4*2, 4+2…)
 
-int(input("Enter 2 variables: ")).split()
-print("a is:",a+b)
-print("b is:",b+ 2*a)
+############PARISA############3
+Task_1 = int(input("Enter your first variable: "))
+Task_2 = int(input("Enter your second variable: "))
+addition = Task_1 + Task_2
+Distributive_property = 2*Task_1 *(Task_1 - Task_2)
+Division = Task_1 / Task_2
+
+print ("addition: " , addition)
+print ("Distributive_property: " , Distributive_property)
+print ("Division: " , Division)

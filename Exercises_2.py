2211752f83e6cf5c9251d3d409074b8eb5ff6004
parisasaad <58298188+parisasaad@users.noreply.github.com>
2022-05-03 19:43:22 -0400

    Add files via upload

---
+++ Exercises_2.py
@@ -0,0 +1,34 @@
+##########Kamyar##########
+val1 = float(input("Enter first variable's value: "))
+val2 = float(input("Enter second variable's value: "))
+minus=val1-val2
+sum=val1+val2
+multiplication=val1*val2
+power=val1**val2
+
+print("minus=",minus)
+print("sum=",sum)
+print("multiplication=",multiplication)
+print("power=",power)
+
+########Parisa########
+val3 = float(input("Enter third variable's value: "))
+minus=minus-val3
+sum=sum+val3
+multiplication=multiplication*val3
+power=power**val3
+
+print(" peer minus=",minus)
+print("peer sum=",sum)
+print("peer multiplication=",multiplication)
+print("peer power=",power)
+
+
+#Ask a user for two inputs
+#Implement  (- , + , *, **, / , operations on them) and print the results.
+#ubmit your code to GitHub Provide access to your peer to modify your code
+#Ask your peer add the third input and apply its value to the output of previous operations, and print it. E.g., (2+2=4  new input Is 2  4*2, 4+2…)
+
+int(input("Enter 2 variables: ")).split()
+print("a is:",a+b)
+print("b is:",b+ 2*a)
